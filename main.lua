fec109ab7fb998946f1660bc8c58f43fce6b85e7
Jukka Jokelainen <jokelainen.jukka@gmail.com>
2013-03-19 15:57:37 +0200

    main.lua skeleton, title and author to conf

--- main.lua
+++ main.lua
@@ -1,1 +1,27 @@
- -- main.lua
+function love.load()
+	love.graphics.setBackgroundColor( 255, 255, 255 )
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.focus(bool)
+end
+
+function love.keypressed( key, unicode )
+end
+
+function love.keyreleased( key, unicode )
+end
+
+function love.mousepressed( x, y, button )
+end
+
+function love.mousereleased( x, y, button )
+end
+
+function love.quit()
+end
