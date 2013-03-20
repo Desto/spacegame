c3141d293188569f64df1b01420ae4c6cf3dd26f
Jukka Jokelainen <jokelainen.jukka@gmail.com>
2013-03-20 11:14:48 +0200

    added planet

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
 	love.graphics.setBackgroundColor( 0, 0, 0 )
+	planet1 = love.graphics.newImage("textures/planet1.png")
 end
 
 function love.draw()
@@ -8,6 +9,9 @@ function love.draw()
 
 	love.graphics.setColor( 255, 255, 255 )
 	love.graphics.rectangle( "fill", x-16, y-16, 32, 32 )
+
+	love.graphics.setColor( 255, 255, 255, 255 )
+	love.graphics.draw(planet1, 300, 300, 0, 1, 1, 0, 0)
 end
 
 function love.update(dt)
