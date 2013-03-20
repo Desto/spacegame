1bfec99dcc0240565929b3cc253da39ece663ea2
Jukka Jokelainen <jokelainen.jukka@gmail.com>
2013-03-20 15:16:49 +0200

    hump implemented

--- main.lua
+++ main.lua
@@ -1,36 +1,34 @@
+Gamestate = require "hump.gamestate"
+
+local menu = {}
+local game = {}
+
+function menu:draw()
+    love.graphics.print("Press Enter to continue", 10, 10)
+end
+
+function menu:keyreleased(key, code)
+    if key == 'enter' then
+        Gamestate.switch(game)
+    end
+end
+
+function game:enter()
+    --Entities.clear()
+    -- setup entities here
+end
+
+function game:update(dt)
+    --Entities.update(dt)
+end
+
+function game:draw()
+    --Entities.draw()
+end
+
 function love.load()
-	love.graphics.setBackgroundColor( 0, 0, 0 )
-	planet1 = love.graphics.newImage("textures/planet1.png")
-end
-
-function love.draw()
-	local x = love.mouse.getX()
-	local y = love.mouse.getY()
-
-	love.graphics.setColor( 255, 255, 255 )
-	love.graphics.rectangle( "fill", x-16, y-16, 32, 32 )
-
-	love.graphics.setColor( 255, 255, 255, 255 )
-	love.graphics.draw(planet1, 300, 300, 0, 1, 1, 0, 0)
-end
-
-function love.update(dt)
-end
-
-function love.focus(bool)
-end
-
-function love.keypressed( key, unicode )
-end
-
-function love.keyreleased( key, unicode )
-end
-
-function love.mousepressed( x, y, button )
-end
-
-function love.mousereleased( x, y, button )
-end
-
-function love.quit()
+	-- love.graphics.setBackgroundColor( 0, 0, 0 )
+	-- planet1 = love.graphics.newImage("textures/planet1.png")
+	Gamestate.registerEvents()
+    Gamestate.switch(menu)
 end
